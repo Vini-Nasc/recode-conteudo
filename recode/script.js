f91f9d1a5aeafe298a6d993f46e3e2b46d5230bd
Vini-Nasc <127364667+Vini-Nasc@users.noreply.github.com>
2025-06-02 22:33:02 -0300

    Add files via upload

---
+++ recode/script.js
@@ -0,0 +1,11 @@
+
+function filtrar(categoria) {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        if (categoria === 'todos' || card.classList.contains(categoria)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+}
